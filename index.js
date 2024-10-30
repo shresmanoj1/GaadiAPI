f55e9f00523795dfbf68cac60ffe340312a66f1f
shresmanoj1 <shrestha.manoj1@googlemail.com>
2024-10-30 17:24:02 +0545

    read

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const server = require('./server')
-
-const port = process.env.PORT || 7777
-
-server.listen(port, () => console.log(`Server running on port ${port}...`))
